a22eb5067ba1994b800bcc145abc072cf4dd2ce5
azf20 <azfuller20@gmail.com>
2025-04-04 14:12:36 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,35 +7,15 @@ const nextConfig = {
         headers: [
           {
             key: "Permissions-Policy",
-            value: "camera=(self), microphone=()",
-          },
-          {
-            key: "Feature-Policy",
-            value: "camera *; microphone 'none'",
-          },
-          {
-            key: "Cross-Origin-Resource-Policy",
-            value: "same-site",
-          },
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "*",
+            value: "camera=*, microphone=*",
           },
           {
             key: "Cross-Origin-Embedder-Policy",
             value: "unsafe-none",
           },
           {
-            key: "Cross-Origin-Opener-Policy",
-            value: "same-origin",
-          },
-          {
-            key: "Cross-Origin-Embedder-Policy",
-            value: "require-corp",
-          },
-          {
-            key: "Permissions-Policy",
-            value: "camera=*, microphone=*",
+            key: "Cross-Origin-Resource-Policy",
+            value: "cross-origin",
           },
         ],
       },
